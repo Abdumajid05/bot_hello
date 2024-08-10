764a2725b1844240143e793136aba95dbba8c622
Abdumajid05 <abdumajidabdulazizov513@gmail.com>
2024-08-10 16:15:43 +0500

    eco bot speech to text

--- get_file.py
+++ get_file.py
@@ -2,14 +2,15 @@
 import requests
 import os
 from pprint import pprint
-
+from send_message import send_message
+import time
 from openai import OpenAI
 
 # Get Open AI API key
 TOKEN_OPENAI = os.environ['TOKEN_OPENAI']
 # Get Telegram API key
 TOKEN = os.environ['TOKEN'] 
-
+URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 def get_file(file_id: str):
     """
     Get file
@@ -64,15 +65,31 @@ def speech_to_text(file_content: bytes):
     
     
 
-file_id = ''
+# # file_id = 'AwACAgIAAxkBAAIBN2a3Eg0juj84eTL7GFsAARFT9ytU1wACM1oAAv8TuUlUoRYWas5p3zUE'
 
 
-file_path = get_file(file_id)['result']['file_path']
-print(file_path)
+# file_path = get_file(file_id)['result']['file_path']
+# print(file_path)
+# file_content = download_file(file_path)
+# text = speech_to_text(file_content)
+# print(text)
 
-file_content = download_file(file_path)
-text = speech_to_text(file_content)
-print(text)
-
-
+next_update_id=0
+chat_id=5423257804
+while True:
+    response = requests.get(URL)
+    data = response.json()
+    result=data['result'][-1]
+    first_update_id=len(data['result'])
+    if first_update_id!=next_update_id:
+        format=list(result['message'])[-1]
+        file_id=result['message'][format]['file_id']
+        file_path = get_file(file_id)['result']['file_path']
+        print(file_path)
+        file_content = download_file(file_path)
+        text = speech_to_text(file_content).text
+        print(text)
+        send_message(chat_id,text)
+    next_update_id=first_update_id
+    time.sleep(1)
 
